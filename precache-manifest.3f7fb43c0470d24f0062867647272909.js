31d40c17cd18a3bee05e04210b74a6a928b719aa
Kovalev Nikita <samec3787@gmail.com>
2020-05-14 17:52:45 +0300

    Updates

---
+++ precache-manifest.3f7fb43c0470d24f0062867647272909.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc918df1465f840e5de7",
+    "url": "/demo-samurai/static/css/main.d8c7f251.chunk.css"
+  },
+  {
+    "revision": "dc918df1465f840e5de7",
+    "url": "/demo-samurai/static/js/main.dc918df1.chunk.js"
+  },
+  {
+    "revision": "236892be778bdd5524ad",
+    "url": "/demo-samurai/static/css/1.95aaad0a.chunk.css"
+  },
+  {
+    "revision": "236892be778bdd5524ad",
+    "url": "/demo-samurai/static/js/1.236892be.chunk.js"
+  },
+  {
+    "revision": "8b123bcf47803d44042c",
+    "url": "/demo-samurai/static/css/2.5b509dca.chunk.css"
+  },
+  {
+    "revision": "8b123bcf47803d44042c",
+    "url": "/demo-samurai/static/js/2.8b123bcf.chunk.js"
+  },
+  {
+    "revision": "e3b2e18d2a7d618c61df",
+    "url": "/demo-samurai/static/css/3.d4db108f.chunk.css"
+  },
+  {
+    "revision": "e3b2e18d2a7d618c61df",
+    "url": "/demo-samurai/static/js/3.e3b2e18d.chunk.js"
+  },
+  {
+    "revision": "452f2e0a293048ac8d43",
+    "url": "/demo-samurai/static/js/4.452f2e0a.chunk.js"
+  },
+  {
+    "revision": "cb4a45af7a56eb0705a0",
+    "url": "/demo-samurai/static/js/runtime~main.cb4a45af.js"
+  },
+  {
+    "revision": "3747db19fe82f1462e06dcad28b1ebb3",
+    "url": "/demo-samurai/index.html"
+  }
+];
